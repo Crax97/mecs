25cdd7c414376e717c9c7771b6b7b07696eb55cb
Crax97 <gsolimeno97@gmail.com>
2025-08-24 20:12:50 +0200

    mecshpp: enum rtti

--- include/mecshpp/mecsrtti.hpp
+++ include/mecshpp/mecsrtti.hpp
@@ -12,16 +12,20 @@
 #define MECS_RTTI_FRIEND(Type) \
     friend struct ::mecs::RTTI<Type>;
 
-#define MECS_RTTI_STRUCT_BEGIN(Type)                                                               \
-    template <>                                                                                    \
-    struct ::mecs::RTTI<Type> {                                                                    \
-        using MecsCurrentT = Type;                                                                 \
-        constexpr static ::mecs::TypeID kTypeID = ::mecs::detail::fnv1a(#Type, sizeof(#Type) - 1); \
-        constexpr static MecsSize kSizeOf = sizeof(Type);                                          \
-        constexpr static MecsSize kAlignOf = alignof(Type);                                        \
-                                                                                                   \
-        constexpr static const char* kName = #Type;                                                \
-        constexpr static auto membersContainer = ::mecs::detail::MemberContainer<0> { }
+#define MECS_RTTI_STRUCT_PRELUDE(Type, Name, TyID, Kind)    \
+    template <>                                             \
+    struct ::mecs::RTTI<Type> {                             \
+        using MecsCurrentT = Type;                          \
+        constexpr static ::mecs::RttiKind kKind = Kind;     \
+        constexpr static ::mecs::TypeID kTypeID = TyID;     \
+        constexpr static MecsSize kSizeOf = sizeof(Type);   \
+        constexpr static MecsSize kAlignOf = alignof(Type); \
+                                                            \
+        constexpr static const char* kName = Name;
+
+#define MECS_RTTI_STRUCT_BEGIN(Type)                                                                                  \
+    MECS_RTTI_STRUCT_PRELUDE(Type, #Type, ::mecs::detail::fnv1a(#Type, sizeof(#Type) - 1), ::mecs::RttiKind::eStruct) \
+    constexpr static auto membersContainer = ::mecs::detail::MemberContainer<0> { }
 
 // NOLINTBEGIN the + is required for syntax masturbation
 #define MECS_RTTI_STRUCT_MEMBER(Name) \
@@ -47,18 +51,49 @@
     }                                                          \
     ;
 
+#define MECS_RTTI_ENUM_BEGIN(Type)                                                                                   \
+    MECS_RTTI_STRUCT_PRELUDE(Type, #Type, ::mecs::typeIdOf<std::underlying_type_t<Type>>(), ::mecs::RttiKind::eEnum) \
+    using UnderlyingType = std::underlying_type_t<Type>;                                                             \
+    constexpr static auto variantsContainer = ::mecs::detail::VariantContainer<UnderlyingType, 0> { }
+
+#define MECS_RTTI_ENUM_VARIANT(VariantValue, Name) \
+    +::mecs::Variant<UnderlyingType> { .name = Name, .value = static_cast<UnderlyingType>(MecsCurrentT::VariantValue) }
+#define MECS_RTTI_ENUM_END()                                      \
+    ;                                                             \
+    constexpr static auto kVariants = variantsContainer.variants; \
+    constexpr static MecsSize kNumVariants = kVariants.size();    \
+    }                                                             \
+    ;
+
 #define MECS_RTTI_SIMPLE(Type)   \
     MECS_RTTI_STRUCT_BEGIN(Type) \
     MECS_RTTI_STRUCT_END()
 
+#define MECS_RTTI_FIELD(Type)                                                                                        \
+    MECS_RTTI_STRUCT_PRELUDE(Type, #Type, ::mecs::detail::fnv1a(#Type, sizeof(#Type) - 1), ::mecs::RttiKind::eField) \
+    constexpr static auto membersContainer = ::mecs::detail::MemberContainer<0> { }                                  \
+    MECS_RTTI_STRUCT_END()
+
 namespace mecs {
 using TypeID = uint64_t;
+
+enum class RttiKind : MecsU8 {
+    eStruct,
+    eEnum,
+    eField,
+};
 
 using GetMemberFn = void* (*)(void*);
 struct Member {
     const char* name;
     TypeID typeID;
     GetMemberFn getMemberFn;
+};
+
+template <typename T>
+struct Variant {
+    const char* name;
+    T value;
 };
 
 namespace detail {
@@ -76,6 +111,15 @@ namespace detail {
         constexpr MemberContainer<nMembers + 1> operator+(const Member& mem)
         {
             return MemberContainer<nMembers + 1> { .members = concat(members, mem, std::make_index_sequence<nMembers>()) };
+        }
+    };
+    template <typename T, MecsSize nMembers>
+    struct VariantContainer {
+        std::array<Variant<T>, nMembers> variants;
+
+        constexpr VariantContainer<T, nMembers + 1> operator+(const Variant<T>& var)
+        {
+            return VariantContainer<T, nMembers + 1> { .variants = concat(variants, var, std::make_index_sequence<nMembers>()) };
         }
     };
 
@@ -134,7 +178,7 @@ using rttiOf = mecs::RTTI<std::remove_const_t<std::remove_reference_t<T>>>;
 
 template <typename T>
 concept HasRTTI = requires {
-    { mecs::rttiOf<T>::componentInfo() } -> std::convertible_to<ComponentInfo>;
+    { mecs::rttiOf<T>::kTypeID } -> std::convertible_to<mecs::TypeID>;
 };
 
 template <typename T>
@@ -145,13 +189,16 @@ constexpr mecs::TypeID typeIdOf()
 }
 }
 
-MECS_RTTI_SIMPLE(int);
-MECS_RTTI_SIMPLE(unsigned int);
-MECS_RTTI_SIMPLE(char);
-MECS_RTTI_SIMPLE(float);
-MECS_RTTI_SIMPLE(double);
-MECS_RTTI_SIMPLE(long);
-MECS_RTTI_SIMPLE(std::string);
+MECS_RTTI_FIELD(int);
+MECS_RTTI_FIELD(unsigned int);
+MECS_RTTI_FIELD(long);
+MECS_RTTI_FIELD(unsigned long);
+MECS_RTTI_FIELD(unsigned long long);
+MECS_RTTI_FIELD(char);
+MECS_RTTI_FIELD(unsigned char);
+MECS_RTTI_FIELD(float);
+MECS_RTTI_FIELD(double);
+MECS_RTTI_FIELD(std::string);
 
 static_assert(mecs::HasRTTI<int>);
 
